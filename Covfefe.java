bf3ab7ff43e4d0e00f6097f4e7e47ffec9f59a15
Grady McPeak <gradymcpeak@gmail.com>
2017-07-12 14:36:27 -0400

    A Robocode robot.
    As an end-of-the-year project, everyone in my class made a robot in Robocode, which we then battled each other in a March Madness-style tournament to crown a champion. Covfefe was not that champion, but nevertheless he earned a respectable third place. Covfefe was named after one of the (many) legendary tweets from the 45th President of the United States. This was originally programmed in the spring of 2017, but I'd like to go back sometime and make some improvements.

---
+++ Covfefe.java
@@ -0,0 +1,48 @@
+package GradysRobots;
+import robocode.*;
+import java.awt.Color;
+
+ public class Covfefe extends AdvancedRobot {
+	 boolean notBroken = true;
+     public void run() {
+		 colors();
+         while (notBroken == true) {
+				move();
+         }
+     }
+	
+	public void move () {
+		setAhead(180);
+		setTurnGunRight(180);
+		GunTurnCompleteCondition y = new GunTurnCompleteCondition (this);
+		waitFor (y);
+		setTurnGunLeft(-180);
+		execute();
+	}
+  
+     public void onScannedRobot(ScannedRobotEvent e) {
+		 setFire(2);  
+	 }
+		 
+     public void onHitWall (HitWallEvent e) {
+		 notBroken = false;
+		 back (20);   
+		 turnRight (e.getBearing());
+		 turnRight (90);
+		 turnGunRight (-1.0 * e.getBearing());
+		 notBroken = true; 
+	 }
+	 
+	public void onHitRobot (HitRobotEvent e) {
+		 turnRight (e.getBearing());
+		 setBack(100);
+		 fire (5);
+		 execute();
+	}
+	
+	public void colors () {
+		setAllColors (Color.black);
+		setScanColor (Color.red);
+		setBulletColor (Color.white);
+	}
+ }
